db4e7508ad1f21fe807252dfff6f05d9f4d7e95f
willan23 <Gnegro401@gmail.com>
2026-06-07 23:04:48 +0100

    fix(deploy): configure outputFileTracingRoot in next.config.ts

--- wgf-senseos/next.config.ts
+++ wgf-senseos/next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingRoot: path.join(__dirname),
   transpilePackages: [
     "@uwsc/core",
     "@uwsc/edge-protocol",
